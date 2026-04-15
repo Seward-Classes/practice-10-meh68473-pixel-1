12f85d5f01dd581ccdc33e9f0513cb6a8890236e
Malaysia Harris <mharris5@ualr.edu>
2026-04-15 19:24:53 +0000

    added rectangle class methods in shape.cpp

--- Shape.cpp
+++ Shape.cpp
@@ -3,3 +3,13 @@
 void Shape::display() const {
     std::cout << "Generic Shape\n";
 }
+
+Rectangle::Rectangle(double w, double h) : width(w), height(h) {}
+
+double Rectangle::getArea() const {
+    return width * height;
+}
+
+void Rectangle::display() const {
+    std::cout << "Rectangle (" << width << " x " << height << ")\n";
+}
