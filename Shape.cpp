96a240fde5afebab1caf411ee6acda78a3c01c98
Malaysia Harris <mharris5@ualr.edu>
2026-04-15 19:27:52 +0000

    fixing mistakes / errors

--- Shape.cpp
+++ Shape.cpp
@@ -1,9 +1,13 @@
 #include "Shape.h"
+#include <cmath>
+#include <iomanip>
 
+// Base display
 void Shape::display() const {
     std::cout << "Generic Shape\n";
 }
 
+// Rectangle
 Rectangle::Rectangle(double w, double h) : width(w), height(h) {}
 
 double Rectangle::getArea() const {
@@ -11,9 +15,11 @@ double Rectangle::getArea() const {
 }
 
 void Rectangle::display() const {
-    std::cout << "Rectangle (" << width << " x " << height << ")\n";
+    std::cout << "Rectangle (" << std::fixed << std::setprecision(1)
+              << width << " x " << height << ")\n";
 }
 
+// Circle
 Circle::Circle(double r) : radius(r) {}
 
 double Circle::getArea() const {
@@ -21,7 +27,6 @@ double Circle::getArea() const {
 }
 
 void Circle::display() const {
-    std::cout << "Circle (radius: " << radius << ")\n";
+    std::cout << "Circle (radius: " << std::fixed << std::setprecision(1)
+              << radius << ")\n";
 }
-
-std::cout << std::fixed << std::setprecision(1);
